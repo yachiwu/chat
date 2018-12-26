36fd57e6c24b7173b2f5ff3819446f2e617facc5
Lucy <n0975116268@gmail.com>
2018-12-27 01:18:10 +0800

    chat format rewriting plus comment

--- chat.py
+++ chat.py
@@ -1,11 +1,10 @@
-def read_file(filename):
+def read_file(filename): #讀取檔案
 	lines = []
 	with open(filename,'r',encoding = 'utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
 	return lines 
-
-def convert(lines):
+def convert(lines): #將檔案的資料做轉換
 	new = []
 	person = None
 	for line in lines:
@@ -18,14 +17,11 @@ def convert(lines):
 		if person:	
 			new.append(person + ' : '+line)	
 	return(new)
-def write_file(filename,lines):
+def write_file(filename,lines): #寫入新的檔案中
 	with open(filename,'w') as f:
 		for line in lines:
 			f.write(line + '\n')
-
-
-
-def main():	
+def main():	#main執行
 	lines = read_file('input.txt')	
 	lines = convert(lines)	
 	write_file('output.txt',lines)
